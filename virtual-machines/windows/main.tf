89683f6d4b613fbd7b10c80df2988c948429d6f6
Ashik Khulal <78129361+ashikkhulal@users.noreply.github.com>
2021-07-03 18:08:09 -0500

    Update main.tf

--- virtual-machines/windows/main.tf
+++ virtual-machines/windows/main.tf
@@ -97,37 +97,6 @@ resource "azurerm_network_interface_security_group_association" "nic-nsg" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-/*
-# Generate a random text for a unique storage account name
-resource "random_id" "randomId" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
-}
-
-# Create a storage account for boot diagnostics
-resource "azurerm_storage_account" "storage" {
-  name                     = "diag${random_id.randomId.hex}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "production"
-  }
-}
-
-# Create a SSH key
-resource "tls_private_key" "win_ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-*/
-
 # Create a windows virtual machine
 
 resource "azurerm_windows_virtual_machine" "winvm" {
@@ -158,20 +127,6 @@ resource "azurerm_windows_virtual_machine" "winvm" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-
-
-
-
-  /*
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.win_ssh.public_key_openssh
-  }
-  
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.storage.primary_blob_endpoint
-  }
-  */
 
   tags = {
     environment = "production"
